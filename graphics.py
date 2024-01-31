16e82b265d9efd45720fe84204bb2d24ea095bdf
Triloq <25014506+Triloq@users.noreply.github.com>
2024-01-31 13:47:42 -0600

    Added Point(), Line() classes

--- graphics.py
+++ graphics.py
@@ -24,20 +24,20 @@ class Window:
         self.running = False
 
     def draw_line(self, line, fill_color):
-        line.draw()
+        line.draw(self.canvas, fill_color)
 
 class Point():
     def __init__(self, x, y):
-        self.__x = x
-        self.__y = y
+        self.x = x
+        self.y = y
 
 class Line():
     def __init__(self, point1, point2):
-        self.__point1 = point1
-        self.__point2 = point2
+        self.point1 = point1
+        self.point2 = point2
 
     def draw(self, canvas, fill_color):
         canvas.create_line(
-            x1, y1, x2, y2, fill=fill_color, width=2
+            self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill=fill_color, width=2
         )
 
--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-from graphics import Window
+from graphics import Window, Line, Point
 
 def main():
     win = Window(800, 600)
+    point1,point2 = Point(500,500), Point(750,750)
+    point3, point4 = Point(0,0), Point(800, 600)
+    line1 = Line(point1, point2)
+    line2 = Line(point3, point4)
+    win.draw_line(line1, 'green')
+    win.draw_line(line2, 'red')
     win.wait_for_close()
 
 main()
